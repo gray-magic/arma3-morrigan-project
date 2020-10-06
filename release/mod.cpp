dac4142dd8269860686c0aba1d0e1651f1c89382
Loic Gasnier <72449447+loic-gasnier@users.noreply.github.com>
2020-10-06 15:45:59 +0200

    Release exe
    Exe for call API in game

---
+++ release/mod.cpp
@@ -0,0 +1,5 @@
+name = "Morrigan";
+description = "Morrigan : Système d'automatisation du champ de bataille";
+actionName = "Website";
+action = "http://www.analyse-innovation-solution.fr/arma3/morrigan";
+author = "Loïc Gasnier";
